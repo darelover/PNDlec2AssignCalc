97f6388c7f5306ab8227a29584977ec79517e2c1
darelover <email>
2017-08-19 23:19:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/atulsachdeva/pndlec2assigncalc/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.atulsachdeva.pndlec2assigncalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static java.lang.Double.sum;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvInputOne, tvInputTwo, tvResult;
+    Button btnAdd, btnSubtract, btnMultiply, btnDivide;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInputOne = (TextView) findViewById(R.id.tvInputOne);
+        tvInputTwo = (TextView) findViewById(R.id.tvInputTwo);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSubtract = (Button) findViewById(R.id.btnSubtract);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnDivide = (Button) findViewById(R.id.btnDivide);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double inputOne = Double.parseDouble(tvInputOne.getText().toString());
+                Double inputTwo = Double.parseDouble(tvInputTwo.getText().toString());
+
+                Double result = Double.sum(inputOne,inputTwo);
+                tvResult.setText(result.toString());
+            }
+        });
+    }
+}
